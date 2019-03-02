131e82f4749b6c043f624fc0111e8f64ec3d6ee2
NeroYang <nerosoft@outlook.com>
2019-03-02 23:47:23 +0800

    bin file read

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include "include/StackVM.h"
+#include <iostream>
+#include <fstream>
 
-int main(){
-    vm::StackVM vm;
+
+int main(int argc, char *argv[]){
+    if(argc <=1){
+        std::cout<<"no file input."<<std::endl;
+        return 0;
+    }
+
+    std::ifstream binFile;
+    binFile.open(argv[1], std::ios::binary | std::ios::in);
+    if(!binFile){
+        std::cout<<"can not open file "<<argv[1]<<std::endl;
+    }
 
     std::vector<vm::i32> prog;
+    vm::i32 code;
+    while(binFile.read((char*)&code, sizeof(code))) { 
+        int readedBytes = binFile.gcount();
+        prog.push_back(code);
+    }
+    binFile.close();
 
-    prog.push_back(0b01000000000000000000000000000111); // push 7
-    prog.push_back(0b01000000000000000000000000000011); // push 3
-    prog.push_back(0b11000000000000000000000000000111); // add
-    prog.push_back(0b01000000000000000000000000000100); // push 4
-    prog.push_back(0b11000000000000000000000000000011); // mul
-    prog.push_back(0b11000000000000000000000000000000); // halt
+    vm::StackVM vm;
+
+    // std::vector<vm::i32> prog;
+
+    // prog.push_back(0b01000000000000000000000000000111); // push 7
+    // prog.push_back(0b01000000000000000000000000000011); // push 3
+    // prog.push_back(0b11000000000000000000000000000111); // add
+    // prog.push_back(0b01000000000000000000000000000100); // push 4
+    // prog.push_back(0b11000000000000000000000000000011); // mul
+    // prog.push_back(0b11000000000000000000000000000000); // halt
 
     vm.loadProgram(prog);
     vm.run();
